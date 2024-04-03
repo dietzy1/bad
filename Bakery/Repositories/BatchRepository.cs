2e312b8509641077555dd2b09b195d24b040fa7a
Asger Nohns <asger@nohns.dk>
2024-04-04 00:01:38 +0200

    feat(repo): batch by id

--- Bakery/Repositories/BatchRepository.cs
+++ Bakery/Repositories/BatchRepository.cs
@@ -6,6 +6,10 @@ namespace Bakery.Repositories;
 public class BatchRepository : Repository {
     public BatchRepository(BakeryContext dbContext) : base(dbContext) { }
 
+    public async Task<Batch?> GetBatchById(int id) {
+        return await Context.Batches.FindAsync(id);
+    }
+
     public async Task<double?> GetAverageDelay() {
         var batches = await Context.Batches.ToListAsync();
         return batches.Average(b => b.Delay);
