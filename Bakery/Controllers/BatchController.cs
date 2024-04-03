36db265327fd3e7df182bb53657e1fe720aaa1e0
Jonas Hjortshøj Rysz <202205608@post.au.dk>
2024-04-03 14:12:17 +0200

    added a bunch of controllers as well as some initial endpoints

---
+++ Bakery/Controllers/BatchController.cs
@@ -0,0 +1,17 @@
+﻿using Bakery.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatchController : ControllerBase
+    {
+        private readonly BakeryContext _context;
+        public BatchController(BakeryContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Bakery/Controllers/IngredientController.cs
@@ -0,0 +1,58 @@
+﻿using Bakery.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Bakery.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class IngredientController : ControllerBase
+    {
+        private readonly BakeryContext _context;
+        public IngredientController(BakeryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetIngredients(string select)
+        {
+            try
+            {
+                IEnumerable<Ingredient> ingredients = _context.Ingredients;
+
+                if (string.IsNullOrEmpty(select))
+                {
+                    return Ok(ingredients);
+                }
+
+                var selectedIngredients = new List<object>();
+                foreach (var ingredient in ingredients)
+                {
+                    var selectedProperties = new Dictionary<string, object>();
+                    var properties = select.Split(',');
+                    foreach (var property in properties)
+                    {
+                        switch (property.Trim().ToLower())
+                        {
+                            case "name":
+                                selectedProperties["Name"] = ingredient.Name;
+                                break;
+                            case "quantity":
+                                selectedProperties["Quantity"] = ingredient.Amount;
+                                break;
+                        }
+                    }
+                    selectedIngredients.Add(selectedProperties);
+                }
+
+                return Ok(selectedIngredients);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
