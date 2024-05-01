61f3215b38b981f5af1d7f7dd4aa3b615bb3b601
jrysz <jonas.rysz@gmail.com>
2024-05-01 18:08:37 +0200

    tried fixing there being no logs

--- Bakery/Controllers/LogController.cs
+++ Bakery/Controllers/LogController.cs
@@ -3,6 +3,8 @@ using Bakery.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Exception = System.Exception;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Bakery.Controllers
 {
@@ -12,28 +14,35 @@ namespace Bakery.Controllers
     public class LogController(LogRepository logRepository) : ControllerBase
     {
         [HttpGet]
-        [Authorize(Policy = "Admin")]
-        public async Task<ActionResult> GetLogs(string userId = null, DateTime? startTime = null, DateTime? endTime = null, string operationType = null)
-       {
-           try
-           {
-               var logs = await logRepository.GetLogs(userId, startTime, endTime, operationType);
-               return Ok(logs);
-           }
-           catch (Exception ex)
-           {
-               return StatusCode(500, ex.Message);
-           }
-       }
+        public async Task<ActionResult<IEnumerable<LogEntry>>> GetLogs(string userId = null, DateTime? startTime = null, DateTime? endTime = null, string operationType = null)
+        {
+            try
+            {
+                var logs = await logRepository.GetLogs(userId, startTime, endTime, operationType);
+                return Ok(logs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
 
 
 
 //We need to add this somewhere idk DTO? Models to filter out shit
+
+[BsonIgnoreExtraElements]
 public class LogEntry
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string UserId { get; set; }
-    public DateTime TimeStamp { get; set; }
+    [BsonElement("Timestamp")]
+    public DateTime Timestamp { get; set; }
+    [BsonElement("Operation Type")]
     public string OperationType { get; set; }
+    [BsonElement("Endpoint")]
+    public string Endpoint { get; set; }
 }
--- Bakery/Repositories/LogRepository.cs
+++ Bakery/Repositories/LogRepository.cs
@@ -13,36 +13,44 @@ public class LogRepository
     public LogRepository(string connectionString)
     {
         var mongoUrl = new MongoUrl(connectionString);
-        var databaseName = mongoUrl.DatabaseName;
-        
         var client = new MongoClient(mongoUrl);
-        var database = client.GetDatabase(databaseName);
+        var database = client.GetDatabase(mongoUrl.DatabaseName);
         _logCollection = database.GetCollection<LogEntry>("logs");
-
     }
 
     //Filter for specific user, timeInterval, and type of operation, post, put, delete
     public async Task<IEnumerable<LogEntry>> GetLogs(string userId = null!, DateTime? startTime = null, DateTime? endTime = null, string operationType = null)
     {
+        Console.WriteLine("TESTING");
+
         var builder = Builders<LogEntry>.Filter;
         var filter = builder.Empty;
 
-        if (!string.IsNullOrEmpty(userId))
-        {
-            filter &= builder.Eq(le => le.UserId, userId);
-        }
+        var logs = _logCollection.Find(log => true);
+        var count = logs.CountDocuments();
 
-        if (startTime.HasValue && endTime.HasValue)
-        {
-            filter &= builder.Gte(le => le.TimeStamp, startTime.Value);
-            filter &= builder.Lte(le => le.TimeStamp, endTime.Value);
-        }
+        Console.WriteLine("number of documents found: " + count);
+        return await logs.ToListAsync();
 
-        if (!string.IsNullOrEmpty(operationType))
-        {
-            filter &= builder.Eq(le => le.OperationType, operationType);
-        }
-
-        return await _logCollection.Find(filter).ToListAsync();
+        //return logs;
     }
+
+    /*
+    if (!string.IsNullOrEmpty(userId))
+    {
+        filter &= builder.Eq(le => le.UserId, userId);
+    }
+    if (startTime.HasValue && endTime.HasValue)
+    {
+        filter &= builder.Gte(le => le.TimeStamp, startTime.Value);
+        filter &= builder.Lte(le => le.TimeStamp, endTime.Value);
+    }
+    if (!string.IsNullOrEmpty(operationType))
+    {
+        filter &= builder.Eq(le => le.OperationType, operationType);
+    }
+
+    var logs = await _logCollection.Find(filter).ToListAsync();
+    return logs;
+    */
 }
